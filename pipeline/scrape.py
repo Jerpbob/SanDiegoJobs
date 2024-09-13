4a0d59cd74af009ded0331a8871ac7afc241ace4
JerpBob <jerupaulbalares@gmail.com>
2024-09-12 23:01:53 -0700

    Added function to scrape the html file for each pageand write it to their respective html files in the raw_html folder

--- pipeline/scrape.py
+++ pipeline/scrape.py
@@ -5,6 +5,10 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://axos.wd5.myworkdayjobs.com/en-US/Axos')
 time.sleep(5) # Let the user actually see something!
-button = driver.find_elements(By.CSS_SELECTOR, 'button[class="css-1jrbae2"]')[0].click()
+with open('./raw_html/html1.html', 'w') as f:
+    f.write(driver.page_source)
+button = driver.find_elements(By.CSS_SELECTOR, 'button[aria-label="page 2"]')[0].click()
+with open('./raw_html/html2.html', 'w') as f:
+    f.write(driver.page_source)
 time.sleep(5) # Let the user actually see something!
 driver.quit()
